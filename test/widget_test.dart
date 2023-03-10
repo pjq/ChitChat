261d0a470a3fe546ec22ac6f95dacc5a823b6b32
Jianqing Peng <Jianqing.Peng@sap.com>
2023-03-10 11:41:58 +0800

    update the app name for Android

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:chitchat/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:chitchat/settings.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final prefs = await SharedPreferences.getInstance();
+    final settings = Settings(prefs: prefs);
+    await tester.pumpWidget(MyApp(settings: settings,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
