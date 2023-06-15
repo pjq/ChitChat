a59234fe6f777058e8ea45f9b1a92b192a9f1c02
Jianqing Peng <pengjianqing@gmail.com>
2023-06-15 18:17:26 +0800

    update author

--- lib/models/pubspec.dart
+++ lib/models/pubspec.dart
@@ -1,6 +1,6 @@
 class Pubspec {
   static const String version = '1.0.0-dev';
   static var versionBuild = '1.0.0-build';
-  static var authorsName = ["Jianqing Peng"];
+  static var authorsName = ["Jianqing Peng", "Freddy Xu"];
   static String description = "The ChitChat application is a fun way to explore and unleash your potential, and it support Android/iOS/Mac/Linux/Web/Windows.";
 }
