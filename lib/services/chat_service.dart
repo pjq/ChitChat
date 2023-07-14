2cfda9c1455c229c5c89c0dc124b8e1a0e0846e9
Jianqing Peng <pengjianqing@gmail.com>
2023-07-14 14:02:58 +0800

    migrate the api request to open ai sdk

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:chitchat/utils/log_utils.dart';
-import 'package:chitchat/models/constants.dart';
 import 'package:chitchat/models/chat_message.dart';
 import 'package:chitchat/models/prompt.dart';
 import 'package:chitchat/models/settings.dart';
 import 'dart:io';
 
+import 'package:dart_openai/dart_openai.dart';
+
 class ChatService {
   late String? apiKey;
   final StreamController<ChatMessage> messageController;
-  final HttpClient client = HttpClient();
-
+  bool useOpenaiSDK = true;
   ChatService(this.messageController);
 
   Future<String> getTranslation(
@@ -19,7 +19,7 @@ class ChatService {
     Settings settings,
   ) async {
     apiKey = settings.openaiApiKey;
-    final response = await getCompletionRaw(
+    final response = await getCompletionRawWithOpenAISDK(
       "",
       translationPrompt,
       settings.temperatureValue,
@@ -38,19 +38,115 @@ class ChatService {
   Future<String> getCompletion(String content, List<ChatMessage>? latestChat,
       Settings? settings, PromptStorage? promptStorage) async {
     apiKey = settings?.openaiApiKey;
-    final response = await getCompletionRaw(
-      content,
-      promptStorage?.getSelectedPrompt().content ?? settings!.promptString,
-      settings!.temperatureValue,
-      latestChat,
-      settings.proxyUrl,
-      settings.baseUrl,
-      settings.selectedModel,
-      settings.streamModeEnable,
-    );
-    LogUtils.info(response);
 
-    return response;
+    if(useOpenaiSDK) {
+      final response = await getCompletionRawWithOpenAISDK(
+        content,
+        promptStorage?.getSelectedPrompt().content ?? settings!.promptString,
+        settings!.temperatureValue,
+        latestChat,
+        settings.proxyUrl,
+        settings.baseUrl,
+        settings.selectedModel,
+        settings.streamModeEnable,
+      );
+      LogUtils.info(response);
+
+      return response;
+    } else {
+      final response = await getCompletionRaw(
+        content,
+        promptStorage?.getSelectedPrompt().content ?? settings!.promptString,
+        settings!.temperatureValue,
+        latestChat,
+        settings.proxyUrl,
+        settings.baseUrl,
+        settings.selectedModel,
+        settings.streamModeEnable,
+      );
+      LogUtils.info(response);
+
+      return response;
+    }
+  }
+
+  Future<String> getCompletionRawWithOpenAISDK(
+      String content,
+      String prompt,
+      double temperatureValue,
+      List<ChatMessage>? latestChat,
+      String proxy,
+      String baseUrl,
+      String aiModel,
+      bool useStream,
+      ) async {
+    LogUtils.info("getCompletionRawWithOpenAISDK");
+    OpenAI.apiKey= apiKey!;
+    OpenAI.baseUrl = baseUrl!;
+
+    // Build the list of chat messages to include in the request body
+    final chatMessages = latestChat?.map((message) {
+          return OpenAIChatCompletionChoiceMessageModel(
+              role: message.isUser
+                  ? OpenAIChatMessageRole.user
+                  : OpenAIChatMessageRole.assistant,
+              content: message.content);
+        }).toList() ??
+        [];
+
+    //add the prompt set as system role
+    if (prompt.isNotEmpty) {
+      chatMessages.insert(0, OpenAIChatCompletionChoiceMessageModel(role: OpenAIChatMessageRole.system, content: prompt));
+    }
+    // Add the user's message to the list of chat messages
+    if (content.isNotEmpty) {
+      chatMessages.add(OpenAIChatCompletionChoiceMessageModel(role: OpenAIChatMessageRole.user, content: content));
+    }
+
+    if(useStream) {
+      Stream<OpenAIStreamChatCompletionModel> chatStream = OpenAI.instance.chat.createStream(
+        model: aiModel,
+        temperature: temperatureValue,
+        messages: chatMessages,
+      );
+
+      chatStream.listen((chatStreamEvent) {
+        // print(chatStreamEvent); // ...
+        final finishReason = chatStreamEvent.choices.last.finishReason;
+        final role = chatStreamEvent.choices.last.delta.role;
+        final returnContent = chatStreamEvent.choices.last.delta.content;
+        ChatMessage chatMessage;
+        if (ChatMessage.STOP == finishReason) {
+          // reach the stop item
+          chatMessage = ChatMessage(
+              role: ChatMessage.ROLE_ASSISTANT, content: ChatMessage.STOP);
+          messageController.add(chatMessage);
+          return;
+        }
+        if (ChatMessage.ROLE_ASSISTANT == role) {
+          // it's the first data.
+          // LogUtils.info("role is assistant");
+        } else {
+          if (null != content) {
+            // The chunk message will be here
+            // chatMessage.content = content;
+            chatMessage =
+                ChatMessage(role: ChatMessage.ROLE_ASSISTANT, content: returnContent!);
+            messageController.add(chatMessage);
+          }
+        }
+      });
+
+    } else {
+      OpenAIChatCompletionModel chatStream = OpenAI.instance.chat.create(
+        model: aiModel,
+        temperature: temperatureValue,
+        messages: chatMessages,
+      ) as OpenAIChatCompletionModel;
+      //
+      return chatStream.choices[0].message.content;
+    }
+    return "";
   }
 
   Future<String> getCompletionRaw(
@@ -63,6 +159,10 @@ class ChatService {
       String aiModel,
       bool useStream,
   ) async {
+    if (useOpenaiSDK) {
+      return getCompletionRawWithOpenAISDK(content, prompt, temperatureValue, latestChat, proxy, baseUrl, aiModel, useStream);
+    }
+
     LogUtils.info("getCompletion");
 
     // Build the list of chat messages to include in the request body
@@ -97,6 +197,7 @@ class ChatService {
     LogUtils.info(body);
 
     LogUtils.info(proxy);
+    final HttpClient client = HttpClient();
     if (proxy.isNotEmpty) {
       client.findProxy = (url) {
         return HttpClient.findProxyFromEnvironment(url,
@@ -110,7 +211,7 @@ class ChatService {
     }
     LogUtils.info(url);
 
-    client.connectionTimeout = const Duration(seconds:60);
+    client?.connectionTimeout = const Duration(seconds:60);
     final request = await client.postUrl(Uri.parse('$url/v1/chat/completions'));
     request.headers.contentType = ContentType.json;
     request.headers.set('Authorization', 'Bearer $apiKey');
